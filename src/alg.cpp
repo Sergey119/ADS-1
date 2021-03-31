5cfb8562722b9f30f59f8bd4c442d2b63c34d9d1
Sergey119 <78952666+Sergey119@users.noreply.github.com>
2021-03-31 18:57:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,33 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-	int count = 0, left = 0, right = size, m, index;
-	while (left <= right) {
-		m = left + ((right - left) / 2);
-		if (value < arr[m]) {
-			right = m - 1;
-		}
-		else if (value > arr[m]) {
-			left = m + 1;
-		}
-		else if (value == arr[m]) {
-			index = m;
-			break;
-		}
-		else {
-			return 0;
-		}
-	}
-	if (index >= 0) {
-		for (int i = index; i >= 0; i--) {
-			if (arr[i] == value) {
-				count++;
-			}
-		}
-	}
-	if (index < size) {
-		for (int i = index + 1; i < size; i++) {
-			if (arr[i] == value) {
-				count++;
-			}
-		}
-	}
-	return count;
+int count = 0, left = 0, right = size, m, index;
+while (left <= right) {
+m = left + ((right - left) / 2);
+if (value < arr[m]) {
+right = m - 1;
+} else if (value > arr[m]) {
+left = m + 1;
+} else if (value == arr[m]) {
+index = m;
+break;
+} else {
+return 0;
+}
+}
+if (index >= 0) {
+for (int i = index; i >= 0; i--) {
+if (arr[i] == value) {
+count++;
+}
+}
+}
+if (index < size) {
+for (int i = index + 1; i < size; i++) {
+if (arr[i] == value) {
+count++;
+}
+}
+}
+return count;
 }
